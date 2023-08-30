5b2d6cbf4981d281b81ddbb2ad5bf263fc3675b5
anthony <anthonymirandagil@gmail.com>
2023-08-29 19:20:28 -0500

    []

--- video_cap.py
+++ video_cap.py
@@ -5,15 +5,17 @@ import os
 path_videos = "./videos"
 os.makedirs(path_videos)
 
-video= cv2.VideoCapture(0)
-width= int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f"\nResolution Video:({width},{height})")
 timestr =  time.strftime("%Y%m%d-%H%M%S")
 writer= cv2.VideoWriter(f'{path_videos}/video_{timestr}.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 10, (width,height))
 
 while True:
-    ret,frame= video.read()
+    ret,frame= cap.read()
 
     writer.write(frame)
     cv2.imshow('frame', frame)
@@ -21,6 +23,6 @@ while True:
     if cv2.waitKey(100) & 0xFF == ord('s'):
         break
 
-video.release()
+cap.release()
 writer.release()
 cv2.destroyAllWindows()
