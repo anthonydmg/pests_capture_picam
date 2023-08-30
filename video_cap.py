c0fdf767607eff06cb695db37a1bb942a095528a
anthony <anthonymirandagil@gmail.com>
2023-08-29 19:00:43 -0500

    []

--- video_cap.py
+++ video_cap.py
@@ -1,21 +1,24 @@
 import cv2
 import time
-video= cv2.VideoCapture(0)
+import os
 
+path_videos = "./videos"
+os.makedirs(path_videos)
+
+video= cv2.VideoCapture(0)
 width= int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height= int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-writer= cv2.VideoWriter('video_exmaple.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
-
+print(f"Resolution Video:({width},{height})")
+timestr =  time.strftime("%Y%m%d-%H%M%S")
+writer= cv2.VideoWriter(f'{path_videos}/video_{timestr}.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 10, (width,height))
 
 for i in range(100):
     ret,frame= video.read()
 
     writer.write(frame)
-    time.sleep(0.2)
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
+    if cv2.waitKey(100) & 0xFF == ord('s'):
         break
 
 
