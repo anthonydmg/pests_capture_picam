a88ebeff7727ea2bd4984ac21dfa0b50be6617cf
anthony <anthonymirandagil@gmail.com>
2023-08-29 19:39:27 -0500

    []

--- capture_images_picamare.py
+++ capture_images_picamare.py
@@ -1,10 +1,19 @@
 from picamera import PiCamera
+import time
 from time import sleep
+import os
+
+os.makedirs("./images", exist_ok=True)
 
 camera = PiCamera()
 camera.resolution = (2592, 1944)
+camera.framerate = 30
+camera.iso = 100
 
-camera.start_preview()
-camera.capture('./image_capture.jpg')
-sleep(5)
-camera.stop_preview()
+sleep(2)
+for filename in camera.capture_continuous('/images/img{counter:03d}.jpg'):
+    print('Captured %s' % filename)
+    sleep(300) # wait 5 minutes
+
+
+
